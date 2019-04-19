e780a15f706c26a7f3e97d479cc1ad20d971ad0c
Avicenna Wisesa <avcwisesa@gmail.com>
2019-04-19 15:59:36 +0700

    created domain model

---
+++ model/model.go
@@ -0,0 +1,19 @@
+package model
+
+import (
+	"time"
+	"github.com/jinzhu/gorm"
+)
+
+type ExchangeRate struct {
+	From string `json:"from" gorm:"primary_key"`
+	To string 	`json:"to" gorm:"primary_key"`
+	Date time.Time  `json:"date" gorm:"primary_key"`
+	Rate float64 `json:"rate"`
+}
+
+type TrackedExchange struct {
+	User string `json:"user" gorm:"primary_key"`
+	From string `json:"from" gorm:"primary_key"`
+	To string 	`json:"to" gorm:"primary_key"`
+}
